36366e4b5d7b5cdff0824ebc9a4046baf4605af5
lxw-peter <1270542959@qq.com>
2022-10-19 23:33:12 +0800

    chore: config commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,3 @@
+// 如果 eslint 报错，就添加下面这行注释
+/* eslint-env node */
+module.exports = { extends: ['@commitlint/config-conventional'] }
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    eslint({
+      cache: false, // 禁用 eslint 缓存
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    open: true,
+  },
 })
